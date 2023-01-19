050a022e0bce8314c8e78e7c8d3754152535825b
Vlad <vladsiryk95@gmail.com>
2023-01-19 20:18:29 +0200

    Generate admin module with GII
    -Generate blank admin module
    -Generate models for Artricle, Comment and Topic

---
+++ modules/admin/views/comment/_search.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var app\models\CommentSearch $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="comment-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+    <?= $form->field($model, 'text') ?>
+    <?= $form->field($model, 'user_id') ?>
+    <?= $form->field($model, 'comment_id') ?>
+    <?= $form->field($model, 'article_id') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-outline-secondary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+</div>
---
+++ modules/admin/views/comment/index.php
@@ -0,0 +1,48 @@
+<?php
+
+use app\models\Comment;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+/** @var yii\web\View $this */
+/** @var app\models\CommentSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = 'Comments';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="comment-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Comment', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'text',
+            'user_id',
+            'comment_id',
+            'article_id',
+            //'date',
+            //'delete',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, Comment $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                }
+            ],
+        ],
+    ]); ?>
+
+</div>

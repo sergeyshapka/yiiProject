13de91db36399899285397f09213990b2c56ba2e
Anna Zaharcenko <zaharcenkoanna147@gmail.com>
2023-01-19 20:23:39 +0200

    Create user module using GII

---
+++ modules/user/views/user/index.php
@@ -0,0 +1,55 @@
+<?php
+
+use yii\grid\GridView;
+use yii\helpers\Html;
+
+/** @var yii\web\View $this */
+/** @var app\models\UserSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = 'Users';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="user-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+
+        'dataProvider' => $dataProvider,
+
+        'columns' => [
+
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+
+            'name',
+
+            'login',
+
+            'password',
+
+            [
+
+                'format' => 'html',
+
+                'label' => 'Image',
+
+                'value' => function ($data) {
+
+                    return Html::img($data->getImage(), ['width' => 200]);
+
+                }
+
+            ],
+
+            ['class' => 'yii\grid\ActionColumn', 'template' => '{view} {update}'],
+
+        ],
+
+    ]); ?>
+
+</div>

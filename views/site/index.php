475fd4c4af659161e4d718c7b89ba2f9bfc6df4d
Anna Zaharcenko <zaharcenkoanna147@gmail.com>
2023-01-19 20:15:56 +0200

    Install main layout

--- views/site/index.php
+++ views/site/index.php
@@ -1,53 +1,34 @@
-<?php
-
-/** @var yii\web\View $this */
-
-$this->title = 'My Yii Application';
-?>
-<div class="site-index">
-
-    <div class="jumbotron text-center bg-transparent">
-        <h1 class="display-4">Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+<div class="col-md-8">
+    <?php use yii\widgets\LinkPager;
+    foreach ($articles as $article): ?>
+        <article class="post">
+            <div class="post-thumb">
+                <a href=""><img class="img-index" src="<?= $article->getImage() ?> " alt="Image"></a>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+            <div class="post-content">
+                <header class="entry-header text-center text-uppercase">
+                    <h6><a href=""> <?= $article->topic->name; ?></a></h6>
+                    <h1 class="entry-title"><a href=""> <?= $article->title; ?> </a></h1>
+                </header>
+                <div class="entry-content">
+                    <p> <?= mb_strimwidth($article->description, 0, 360, "..."); ?> </p>
+                    <div class="btn-continue-reading text-center text-uppercase">
+                        <a href="#" class="more-link">Continue Reading</a>
+                    </div>
+                </div>
+                <div class="social-share">
+                    <span class="social-share-title pull-left text-capitalize">By <?= $article->user->name; ?> On <?= $article->getDate(); ?></span>
+                    <ul class="text-center pull-right">
+                        <li><a class="s-facebook" href="#"><i class="fa fa-eye"></i></a></li>
+                        <?= (int)$article->viewed; ?>
+                    </ul>
+                </div>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-
-    </div>
+        </article>
+    <?php endforeach;
+        echo LinkPager::widget([
+            "pagination" => $pagination,
+        ])
+    ?>
 </div>
+<?php echo Yii::$app->view->renderFile('@app/views/site/right.php', compact('popular', 'recent', 'topics')); ?>
